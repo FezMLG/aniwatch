aec1838329a93f18b3a820906120374aae1a2222
FezMLG <46359181+FezMLG@users.noreply.github.com>
2022-01-27 00:10:17 +0100

    starting to work on other subs

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import morgan from "morgan";
 
 import cors from "./config/cors";
-import router from './routes/index';
+import router from "./routes/index";
 
 const PORT = 4000;
 
@@ -10,9 +10,10 @@ const app = express();
 
 app.use(cors);
 app.use(express.json());
-app.use(morgan(":method :url :status :response-time ms - :res[content-length] - [:date[clf]]"));
+// app.use(morgan(":method :url :status :response-time ms - :res[content-length] - [:date[clf]]"));
+app.use(morgan("dev"));
 
-app.use('/api/', router);
+app.use("/api/", router);
 
 app.listen(PORT, () => console.log(`The server is running on port ${PORT}`));
 
--- api/src/routes/anime.ts
+++ api/src/routes/anime.ts
@@ -1,10 +1,12 @@
-import express from 'express';
+import express from "express";
 
-import { getAnime, getFromSeason } from '../controllers/anime';
+import { getAnime, getFromSeason } from "../controllers/anime";
+import { getAnimeTest } from "../libs/nanatsusubs";
 
 const router = express.Router();
 
-router.post('/series', getAnime);
-router.post('/season', getFromSeason);
+router.post("/series", getAnime);
+router.post("/season", getFromSeason);
+router.post("/test", getAnimeTest);
 
 export default router;
